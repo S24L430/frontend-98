303f096d510add8c16a55cbf45191ef3635f2892
Quentin Vecchio <qvecchio@adobe.com>
2024-03-20 09:47:18 +0100

    test

--- ui.frontend/src/main/webpack/site/main.js
+++ ui.frontend/src/main/webpack/site/main.js
@@ -1,46 +1,28 @@
-// (() => {
-//   function submitContactUsForm() {
-//     const username = document.getElementById("wknd-contact-us-form-username").value;
-//     const time = Date.now();
-//     const myRequest = new Request("/us/en/about-us.html?username=" + username + "&time=" + time);
+(() => {
+  function submitContactUsForm() {
+    const username = document.getElementById("wknd-contact-us-form-username").value;
+    const time = Date.now();
+    const myRequest = new Request("/us/en/about-us.html?username=" + username + "&time=" + time);
 
-//     fetch(myRequest)
-//         .then((response) => {
-//             if (!response.ok) {
-//                 console.log(response);
-//                 document.getElementById("cmp-contactus-error").innerHTML = "Error: " + response.status;
-//             } else {
-//                 console.log(response.status);
-//                 window.location.href = "/us/en/about-us.html?username=" + username + "&time=" + time;
-//             }
+    fetch(myRequest)
+        .then((response) => {
+            if (!response.ok) {
+                console.log(response);
+                document.getElementById("cmp-contactus-error").innerHTML = "Error: " + response.status;
+            } else {
+                console.log(response.status);
+                window.location.href = "/us/en/about-us.html?username=" + username + "&time=" + time;
+            }
 
-//             return response.blob();
-//         });
-//     }
-
-//     console.log("loaded");
-//     document.getElementById("wknd-contact-us-form-submit").onclick = submitContactUsForm;
-// })();
-
-import jQuery from "jquery";
-
-jQuery(function($) {
-    "use strict";
-
-    $( "#wknd-contact-us-form-submit" ).on( "click", function( event ) {
-        event.preventDefault();
-        const username = $( "#wknd-contact-us-form-username" ).val();
-        const time = Date.now();
-        $.get( `/us/en/about-us.html?username=${username}&time=${time}`, function(data) {
-            console.log(data);
-            window.location.href = `/us/en/about-us.html?username=${username}&time=${time}`;
-          })
-        .fail(function(err) {
-            console.log(err);
-            $( "#cmp-contactus-error" ).html(`Error: ${err.status}`);
+            return response.blob();
         });
+    }
+
+    window.addEventListener('load', function () {
+        console.log("loaded");
+        document.getElementById("wknd-contact-us-form-submit").onclick = submitContactUsForm;
     });
-});
+})();
 
 // Stylesheets
 import './main.scss';
